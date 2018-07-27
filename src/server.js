597f45e0d80697b44948334692d74ece88e292b2
willyb321 <wbwilliam7@gmail.com>
2018-07-27 11:58:16 +1000

    remove boilerplate facebook

--- src/server.js
+++ src/server.js
@@ -180,31 +180,6 @@ app.post(
   (req, res) => res.redirect('/')
 );
 
-app.get(
-  '/login/facebook',
-  passport.authenticate('facebook', {
-    scope: ['email', 'user_location'],
-    session: false
-  })
-);
-
-app.get(
-  '/login/facebook/return',
-  passport.authenticate('facebook', {
-    failureRedirect: '/login',
-    session: false
-  }),
-  (req, res) => {
-    const expiresIn = 60 * 60 * 24 * 180; // 180 days
-    const token = jwt.sign(req.user, config.auth.jwt.secret, { expiresIn });
-    res.cookie('id_token', token, {
-      maxAge: 1000 * expiresIn,
-      httpOnly: true
-    });
-    res.redirect('/');
-  }
-);
-
 //
 // Register API middleware
 // -----------------------------------------------------------------------------
