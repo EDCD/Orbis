e8c831c3121390298142e9d3ceed7b26f1279e06
willyb321 <wbwilliam7@gmail.com>
2018-07-31 16:21:39 +1000

    fix logging

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,8 @@ module.exports = {
 	},
 	production: {
 		username: process.env.DB_USERNAME,
-		password: process.env.DB_PASSWORD,
+    logging: false,
+    password: process.env.DB_PASSWORD,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOSTNAME,
 		dialect: 'mysql',
