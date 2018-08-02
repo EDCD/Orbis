5d8ab55ce0cec156baf2bd1e18ea15f4fa6d7946
willyb321 <wbwilliam7@gmail.com>
2018-08-03 09:59:59 +1000

    sw

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
@@ -279,42 +279,54 @@ module.exports = {
 		new ManifestPlugin({
 			fileName: 'asset-manifest.json'
 		}),
-		// Generate a service worker script that will precache, and keep up to date,
-		// the HTML & assets that are part of the Webpack build.
-		new SWPrecacheWebpackPlugin({
-			// By default, a cache-busting query parameter is appended to requests
-			// used to populate the caches, to ensure the responses are fresh.
-			// If a URL is already hashed by Webpack, then there is no concern
-			// about it being stale, and the cache-busting can be skipped.
-			dontCacheBustUrlsMatching: /\.\w{8}\./,
-			filename: 'service-worker.js',
-			logger(message) {
-				if (message.indexOf('Total precache size is') === 0) {
-					// This message occurs for every build and is a bit too noisy.
-					return;
-				}
-				if (message.indexOf('Skipping static resource') === 0) {
-					// This message obscures real errors so we ignore it.
-					// https://github.com/facebookincubator/create-react-app/issues/2612
-					return;
-				}
-				console.log(message);
-			},
-			minify: true,
-			// For unknown URLs, fallback to the index page
-			navigateFallback: publicUrl + '/index.html',
-			// Ignores URLs starting from /__ (useful for Firebase):
-			// https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
-			navigateFallbackWhitelist: [/^(?!.*(api))/],
+		new WorkboxPlugin.GenerateSW({
+			// Exclude images from the precache
+			exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+			// Define runtime caching rules.
 			runtimeCaching: [{
-				urlPattern: /\/api\/.*/,
+				// Match any request ends with .png, .jpg, .jpeg or .svg.
+				urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+				// Apply a cache-first strategy.
+				handler: 'cacheFirst',
+
+				options: {
+					// Use a custom cache name.
+					cacheName: 'images',
+
+					// Only cache 10 images.
+					expiration: {
+						maxEntries: 10
+					}
+				}
+			}, {
+				// Match any same-origin request that contains 'api'.
+				urlPattern: /api/,
+				// Apply a network-first strategy.
 				handler: 'networkFirst',
 				options: {
-					debug: true
+					// Fall back to the cache after 10 seconds.
+					networkTimeoutSeconds: 10,
+					// Use a custom cache name for this route.
+					cacheName: 'orbis-api-cache',
+					// Configure custom cache expiration.
+					expiration: {
+						maxEntries: 5,
+						maxAgeSeconds: 60
+					},
+					// Configure which responses are considered cacheable.
+					cacheableResponse: {
+						statuses: [0, 200],
+						headers: {'x-test': 'true'}
+					},
+					// Configure the broadcast cache update plugin.
+					broadcastUpdate: {
+						channelName: 'my-update-channel'
+					}
 				}
-			}],
-			// Don't precache sourcemaps (they're large) and build asset manifest:
-			staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/]
+
+			}]
 		}),
 		// Moment.js is an extremely popular library that bundles large locale files
 		// by default due to how Webpack interprets its code. This is a practical
