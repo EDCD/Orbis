658fb787567e507edd0c0f5dd320969a13a89e31
willyb321 <wbwilliam7@gmail.com>
2018-08-08 14:42:26 +1000

    format

--- client/src/features/common/Header.js
+++ client/src/features/common/Header.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Navigation from './Navigation';
 import {Link} from 'react-router-dom';
 import logoUrl from './Orbis.png';
 
 export default class Header extends Component {
-  static propTypes = {
+	static propTypes = {};
 
-  };
-
-  render() {
-    return (
-      <header>
-        <Link className={'l'} to="/">
-          <img src={logoUrl} width="38px" height="38px" alt="React" />
-        </Link>
-        <div className={'l menu'}>
-          <div className={'menu-header'}>
-            <Link className={'menu-item-label'} to="/">
-              Orbis.zone
-            </Link>
-          </div>
-        </div>
-        <Navigation />
-      </header>
-    );
-  }
+	render() {
+		return (
+			<header>
+				<Link className={'l'} to="/">
+					<img src={logoUrl} width="38px" height="38px" alt="React"/>
+				</Link>
+				<div className={'l menu'}>
+					<div className={'menu-header'}>
+						<Link className={'menu-item-label'} to="/">
+							Orbis.zone
+						</Link>
+					</div>
+				</div>
+				<Navigation/>
+			</header>
+		);
+	}
 }
