f541f6b139ddb826251ae181da2ac8b7a73f7229
willyb321 <wbwilliam7@gmail.com>
2018-08-19 08:16:52 +1000

    fix sw not precaching

--- client/src/sw.js
+++ client/src/sw.js
@@ -2,6 +2,7 @@ console.log('Hello from sw.js');
 
 if (workbox) {
 	console.log(`Yay! Workbox is loaded 🎉`);
+	workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 	workbox.routing.registerRoute(
 		new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
 		workbox.strategies.cacheFirst({
@@ -17,29 +18,42 @@ if (workbox) {
 		})
 	);
 	workbox.routing.registerRoute(
-		/\.(?:png|gif|jpg|jpeg|svg)$/,
+		// Cache CSS files
+		/.*\.css/,
+		// Use cache but update in the background ASAP
+		workbox.strategies.staleWhileRevalidate({
+			// Use a custom cache name
+			cacheName: 'css-cache'
+		})
+	);
+	workbox.routing.registerRoute(
+		// Cache image files
+		/.*\.(?:png|jpg|jpeg|svg|gif)/,
+		// Use the cache if it's available
 		workbox.strategies.cacheFirst({
-			cacheName: 'images',
+			// Use a custom cache name
+			cacheName: 'image-cache',
 			plugins: [
 				new workbox.expiration.Plugin({
-					maxEntries: 60,
-					maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+					// Cache only 20 images
+					maxEntries: 20,
+					// Cache for a maximum of a week
+					maxAgeSeconds: 7 * 24 * 60 * 60
 				})
 			]
 		})
 	);
+
 	workbox.routing.registerRoute(
-		/\.(?:js|css)$/,
-		workbox.strategies.staleWhileRevalidate({
-			cacheName: 'static-resources'
-		})
+		/.*\.js/,
+		workbox.strategies.networkFirst()
 	);
+
 	workbox.routing.registerRoute(
 		/api/,
 		new workbox.strategies.NetworkFirst({})
 	);
 	workbox.googleAnalytics.initialize();
-
 } else {
 	console.log(`Boo! Workbox didn't load 😬`);
 }
