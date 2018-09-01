16fca3dddbfbf194f7c1369897f2502b0252aca0
willyb321 <wbwilliam7@gmail.com>
2018-09-02 07:51:58 +1000

    more work on embeds

--- server/app.js
+++ server/app.js
@@ -81,6 +81,7 @@ app.get('/build/:id/og', (req, res) =>
 			html += `<meta name="og:image" content="${ship.imageURL}"/>`;
 			html += `<meta name="og:url" content="${uri}"/>`;
 			html += `<meta property="og:type" content="website"/>`;
+			html += `<meta name="twitter:card" content="summary_large_image">`;
 			html += `<meta http-equiv="refresh" content="0;url='${uri}'"/>`;
 			html += `</head>`;
 			html += `<body/>`;
@@ -92,6 +93,30 @@ app.get('/build/:id/og', (req, res) =>
 			res.status(500).end();
 		})
 );
+
+app.get('/build/:id/oembed', (req, res) =>
+	Ship.find({
+		where: {shortid: req.params.id},
+		attributes: ['id', 'updatedAt', 'createdAt', 'shortid', 'title', 'description', 'author', 'imageURL', 'url', 'proxiedImage', 'coriolisShip']
+	})
+		.then(ship => {
+			if (!ship) {
+				return res.json({});
+			}
+			const json = {
+				author_name: 'It\'s time to dock',
+				author_url: 'https://orbis.zone',
+				provider_name: 'Orbis.zone is the build repository for Coriolis.io.',
+				provider_url: 'https://orbis.zone'
+			};
+			return res.send(json);
+		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).end();
+		})
+);
+
 app.use('/api', index);
 app.use('/api/builds', builds);
 app.use('/api/users', users);
