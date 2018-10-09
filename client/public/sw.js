0f18a1fce148ecac161ad161d5388c9d782649f9
William Blythe <william.blythe@intygrate.com>
2018-10-10 09:16:42 +1100

    service worker stuff

---
+++ client/public/sw.js
@@ -0,0 +1,72 @@
+console.log("Hello from sw.js");
+
+if (workbox) {
+	console.log("Yay! Workbox is loaded 🎉");
+	workbox.routing.registerRoute(
+		new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+		workbox.strategies.cacheFirst({
+			cacheName: "google-fonts",
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 30
+				}),
+				new workbox.cacheableResponse.Plugin({
+					statuses: [0, 200]
+				})
+			]
+		})
+	);
+	workbox.routing.registerRoute(
+		/\.(?:png|gif|jpg|jpeg|svg)$/,
+		workbox.strategies.cacheFirst({
+			cacheName: "images",
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+				})
+			]
+		})
+	);
+	workbox.routing.registerRoute(
+		/\.(?:js|css)$/,
+		workbox.strategies.staleWhileRevalidate({
+			cacheName: "static-resources"
+		})
+	);
+	try {
+		workbox.googleAnalytics.initialize();
+	} catch (e) {
+		console.log("Probably an ad-blocker");
+	}
+} else {
+	console.log("Boo! Workbox didn't load 😬");
+}
+(async () => {
+	if (event.request.mode === "navigate" && registration.waiting) {
+		if ((await clients.matchAll()).length < 2) {
+			registration.waiting.postMessage("skipWaiting");
+		}
+	}
+})();
+
+addEventListener("message", messageEvent => {
+	if (messageEvent.data === "skipWaiting") return skipWaiting();
+});
+
+addEventListener("fetch", event => {
+	event.respondWith(
+		(async () => {
+			if (
+				event.request.mode === "navigate" &&
+				event.request.method === "GET" &&
+				registration.waiting &&
+				(await clients.matchAll()).length < 2
+			) {
+				registration.waiting.postMessage("skipWaiting");
+				return new Response("", { headers: { Refresh: "0" } });
+			}
+			return (await caches.match(event.request)) || fetch(event.request);
+		})()
+	);
+});
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,12 +1,22 @@
 module.exports = {
 	pwa: {
-		name: 'Orbis.zone',
-		skipWaiting: true,
-		clientsClaim: true
+		name: "Orbis.zone",
+		themeColor: "#4DBA87",
+		msTileColor: "#000000",
+		appleMobileWebAppCapable: "yes",
+		appleMobileWebAppStatusBarStyle: "black",
+
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: "public/sw.js",
+			swDest: "service-worker.js"
+			// ...other Workbox options...
+		}
 	},
 	devServer: {
-		proxy: 'http://localhost:3030',
+		proxy: "http://localhost:3030",
 		disableHostCheck: true,
-		host: '0.0.0.0'
+		host: "0.0.0.0"
 	}
 };
