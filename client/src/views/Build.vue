a30af7a98d2d24d6ef36f1e2826d45ee82757a2f
William Blythe <william.blythe@intygrate.com>
2018-11-06 09:36:26 +1100

    wrong property

--- client/src/views/Build.vue
+++ client/src/views/Build.vue
@@ -169,7 +169,7 @@
       saveBuildToCoriolis() {
         this.saving = true;
         let builds = JSON.parse(JSON.stringify(this.coriolisBuilds));
-        builds[this.ship.coriolisShip.coriolisId][this.ship.title] = this.shipCode;
+        builds[this.ship.coriolisShip.id][this.ship.title] = this.shipCode;
         xdLocalStorage.setItem('builds', JSON.stringify(builds), (data) => {
           console.log(data);
           this.snackbar = true;
