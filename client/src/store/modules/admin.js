c261bcfa3cac0a30b97d0b70b4357667569aafb6
Willyb321 <wbwilliam7@gmail.com>
2018-11-21 16:23:25 +1100

    add delete announcement

--- client/src/store/modules/admin.js
+++ client/src/store/modules/admin.js
@@ -1,28 +1,44 @@
 import axios from 'axios';
 
 const types = {
-	ADMIN_POST_ANNOUNCEMENT_REQUEST: 'ADMIN_POST_ANNOUNCEMENT_REQUEST'
+  ADMIN_POST_ANNOUNCEMENT_REQUEST: 'ADMIN_POST_ANNOUNCEMENT_REQUEST',
+  ADMIN_DELETE_ANNOUNCEMENT_REQUEST: 'ADMIN_DELETE_ANNOUNCEMENT_REQUEST'
 };
 
 export default {
-	state: {
-		announcement: {}
-	},
-	mutations: {
-		ADMIN_POST_ANNOUNCEMENT_REQUEST(state, data) {
-			state.announcement = data.data;
-		}
-	},
-	actions: {
-		async postAnnouncement({commit}, {expiresAt, message, showInCoriolis}) {
-			commit(types.ADMIN_POST_ANNOUNCEMENT_REQUEST, await axios.post(`/api/admin/announcement/add`, {
-				expiresAt,
-				showInCoriolis,
-				message
-			}, {
-				withCredentials: true
-			}));
-		}
-	},
-	getters: {}
+  state: {
+    announcement: {}
+  },
+  mutations: {
+    ADMIN_POST_ANNOUNCEMENT_REQUEST(state, data) {
+      state.announcement = data.data;
+    },
+    ADMIN_DELETE_ANNOUNCEMENT_REQUEST(state, data) { }
+  },
+  actions: {
+    async postAnnouncement({ commit }, { expiresAt, message, showInCoriolis }) {
+      commit(types.ADMIN_POST_ANNOUNCEMENT_REQUEST, await axios.post(`/api/admin/announcement/add`, {
+        expiresAt,
+        showInCoriolis,
+        message
+      }, {
+          withCredentials: true
+        }));
+    },
+    async deleteAnnouncement({ commit }, { id }) {
+      try {
+        await axios.post(`/api/admin/announcement/delete`, {
+          id
+        }, {
+            withCredentials: true
+          })
+      } catch (e) {
+        if (e.response.status !== 401) {
+          console.log(e);
+        }
+      }
+      commit(types.ADMIN_DELETE_ANNOUNCEMENT_REQUEST, id);
+    }
+  },
+  getters: {}
 };
--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -9,6 +9,9 @@
 			<!--</v-flex>-->
 		<!--</v-layout>-->
 		<add-announcement/>
+    <v-flex xs6 sm12 v-for="announce in $store.state.Common.announcements" :key="announce.id">
+      {{announce.message}} <v-btn @click="() => deleteAnnouncement(announce)">Delete</v-btn>
+    </v-flex>
 		<!--<v-pagination-->
 			<!--v-show="builds"-->
 			<!--v-model="page"-->
@@ -20,51 +23,55 @@
 </template>
 
 <script>
-	import AddAnnouncement from '../components/AddAnnouncement';
+  import AddAnnouncement from '../components/AddAnnouncement';
 
-	export default {
-    components: {AddAnnouncement},
-		data: () => {
-			return {
-				alert: true,
-				page: 1,
-				pageSize: 10,
-				menu: false,
-				announcementPickerVisible: false,
-				announcementText: '',
-				date: null,
-				searchData: {},
-				loading: false
-			};
-		},
-		computed: {
-			builds() {
-				return this.$store.state.Common.builds.rows;
-			},
-			pages() {
-				if (!this.$store.state.Common.builds.count) {
-					return 1;
-				}
-				return Math.ceil(this.$store.state.Common.builds.count / this.pageSize);
-			},
-			offset() {
-				return Math.ceil(this.page * this.pageSize);
-			}
-		},
-		methods: {
-			async search(value) {
-				this.loading = true;
-				this.searchData = value;
-				await this.$store.dispatch('getBuilds', {pageSize: this.pageSize, offset: 0, search: value});
-				this.loading = false;
-			},
-			async paginate() {
-				await this.$store.dispatch('getBuilds', {
-					pageSize: this.pageSize,
-					offset: this.offset,
-					search: this.searchData
-				});
-			}
-		}
-	};
+  export default {
+    components: { AddAnnouncement },
+    data: () => {
+      return {
+        alert: true,
+        page: 1,
+        pageSize: 10,
+        menu: false,
+        announcementPickerVisible: false,
+        announcementText: '',
+        date: null,
+        searchData: {},
+        loading: false
+      };
+    },
+    computed: {
+      builds() {
+        return this.$store.state.Common.builds.rows;
+      },
+      pages() {
+        if (!this.$store.state.Common.builds.count) {
+          return 1;
+        }
+        return Math.ceil(this.$store.state.Common.builds.count / this.pageSize);
+      },
+      offset() {
+        return Math.ceil(this.page * this.pageSize);
+      }
+    },
+    methods: {
+      async search(value) {
+        this.loading = true;
+        this.searchData = value;
+        await this.$store.dispatch('getBuilds', { pageSize: this.pageSize, offset: 0, search: value });
+        this.loading = false;
+      },
+      async deleteAnnouncement(item) {
+        await this.$store.dispatch('deleteAnnouncement', { id: item.id });
+        await this.$store.dispatch('getAnnouncements');
+      },
+      async paginate() {
+        await this.$store.dispatch('getBuilds', {
+          pageSize: this.pageSize,
+          offset: this.offset,
+          search: this.searchData
+        });
+      }
+    }
+  };
 </script>
